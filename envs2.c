13a09a36ebd94fa652bab741cc1ffeb6e5b7a7e8
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-07-27 10:52:31 -0700

    seyenv function

---
+++ envs2.c
@@ -0,0 +1,26 @@
+#include <shell.h>
+
+/**
+ * setenv_func - this function sets the env
+ * @chars_input: shell command
+ * Return: 0
+ */
+
+void setenv_func(char **chars_input)
+{
+	if (strncmp(chars_input, "setenv", 6) == 0)
+	{
+		token1 = strtok(chars_input, delim);
+		if (builtin_checker(token1))
+		{
+			token1 = strtok(NULL, delim);
+			token = strtok(token1, "=");
+			key = token;
+			token = strtok(NULL, "=");
+			value = token;
+			setenv(key, value, 1);
+		}
+	}
+
+	return (0);
+}
