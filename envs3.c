5ee84f37cbafd7e2fb03e33dcecebf0b32d7493b
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-07-27 11:02:55 -0700

    unset env function

---
+++ envs3.c
@@ -0,0 +1,22 @@
+#include <shell.h>
+
+/**
+ * unsetenv_func -  function that unsets the env
+ * @chars_input: shell command
+ * Return: 0
+ */
+
+void unsetenv_func(char **chars_input)
+{
+	if (strncmp(chars_input, "unsetenv", 8) == 0)
+	{
+		token = strtok(command, delim);
+		if (builtin_checker(token))
+		{
+			token = strtok(NULL, delim);
+			unsetenv(token);
+		}
+	}
+
+	return (0);
+}
