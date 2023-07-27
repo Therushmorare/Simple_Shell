e7c3c05031539db6f406e1012f37a17da9968dc8
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-07-27 09:44:07 -0700

    built in function

---
+++ builtins.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *builtin_checker - checks if command is a builtin
+ *@user_command: the command to be checked
+ *Return: zero
+ */
+
+int builtin_checker(const char *user_command)
+{
+	int i;
+	const char *builtins[] = {
+	"env", "setenv", "exit", "unsetenv"
+	};
+
+	for (int i = 0; i < sizeof(builtins) / sizeof(builtins[0]); ++i)
+	{
+		if (strcmp(user_command, builtins[i]) == 0)
+		{
+			return (1);
+			exit(127);
+		}
+	}
+
+	return (0);
+}
